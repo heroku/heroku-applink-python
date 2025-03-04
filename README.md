c5d593fa3c0f37607239e3ded7c2c24d7354383c
sfdcarter <christian.carter@salesforce.com>
2025-03-04 09:49:59 -0800

    initial import

---
+++ README.md
@@ -0,0 +1,10 @@
+# Python SDK for Heroku Integrations
+
+This library provides basic functionality for building Python apps that use
+Heroku Integrations to secure communication and credential sharing with a Salesforce
+org.
+
+Though the interaction with Integrations is simple and easy to hand code, using the
+SDK will quickstart your project experience.
+
+Use of this project with Salesforce is subject to the TERMS_OF_USE.md document.
---
+++ examples/fastapi_example.py
@@ -0,0 +1,25 @@
+import asyncio
+import salesforce_sdk as sdk
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+app.add_middleware(sdk.IntegrationAsgiMiddleware)
+
+
+@app.get("/")
+def get_root():
+    return {"root": "page"}
+
+
+@app.get("/accounts")
+def get_accounts():
+    dataapi = sdk.context.get()
+    asyncio.run(query_accounts(dataapi))
+    return {"Some": "Accounts"}
+
+
+async def query_accounts(dataapi):
+    query = "SELECT Id, Name FROM Account"
+    result = await dataapi.query(query)
+    for record in result.records:
+        print("===== account record", record)
