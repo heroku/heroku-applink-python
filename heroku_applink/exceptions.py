45a1f0c2188d2c2dbaec0658fa473f868e3b557c
Jon Daniel <jon.daniel@salesforce.com>
2025-05-28 12:38:10 -0400

    Adding copyright header to files

--- heroku_applink/exceptions.py
+++ heroku_applink/exceptions.py
@@ -1,4 +1,9 @@
-# heroku_applink/exceptions.py
+"""
+Copyright (c) 2025, salesforce.com, inc.
+All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
+For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
 
 class ClientError(Exception):
     """Raised when there is an error with the HTTP client."""
