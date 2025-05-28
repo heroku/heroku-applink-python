4bc10fc3f0a2289571d63630a731fe3183d1c40b
Jon Daniel <jon.daniel@salesforce.com>
2025-05-28 12:39:21 -0400

    Adding copyright header to more files

--- heroku_applink/data_api/_requests.py
+++ heroku_applink/data_api/_requests.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2025, salesforce.com, inc.
+All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause
+For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
 from base64 import standard_b64encode
 from typing import Any, Awaitable, Callable, Generic, Literal, TypeVar, cast
 from urllib.parse import urlencode
